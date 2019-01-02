23eb9aa94180340450bcaee84fc4a57ac8a3a824
Tom Andersson <you@example.com>
2019-01-02 22:48:53 +0000

    Changed input type in SchwefelFunction from primitive array to Collections 'ArrayList'

--- Java project/src/tabusearch/SchwefelFunction.java
+++ Java project/src/tabusearch/SchwefelFunction.java
@@ -1,32 +1,31 @@
 package tabusearch;
 
 import java.lang.Math;
+import java.util.ArrayList;
 
 // Class to evaluate the Schwefel Function of dimension N
 
 public class SchwefelFunction {
 	
 	private int N; // Number of dimensions of the Schwefel function
-	public double[] x; // Input point location
 	
 	public SchwefelFunction(int N) {
 		this.N = N;
 	} // Constructor to set the number of dimensions for the object
 	
-	public double f(double[] x) {
+	public double f(ArrayList<Double> x) {
 		double funcval = 0.0;
 		int N = this.N;
 		
-		if (x.length == N) {
-			for (int i = 0; i < N; i++) {
-				funcval = funcval - x[i]*Math.sin(Math.sqrt(Math.abs(x[i])));
+		if (x.size() == N) {
+			for (Double xi : x) {
+				funcval = funcval - xi*Math.sin(Math.sqrt(Math.abs(xi)));
 			}
 		}
 		else {
 			System.out.println("Error: Dimension of input point must match the Schwefel Function dimension.");
 			System.exit(0);
 		}
-		
 		
 		return funcval;
 	} // Method to evaluate and return the Schwefel function.
--- Java project/src/tabusearch/TSMain.java
+++ Java project/src/tabusearch/TSMain.java
@@ -1,5 +1,7 @@
 package tabusearch;
 
+import java.util.ArrayList;
+
 // Perform the Tabu Search algorithm on the Schwefel Function
 
 public class TSMain {
@@ -9,7 +11,10 @@ public class TSMain {
 		
 		SchwefelFunction schwef = new SchwefelFunction(2); // Create SchwefelFunction object of dim
 		
-		double[] x = {1.2,-4.3};
+		ArrayList<Double> x = new ArrayList<Double>();
+		x.add(1.2);
+		x.add(-4.3);
+		
 		double funcval = schwef.f(x);
 		
 		System.out.println("The Schwefel function value is: " + funcval);
