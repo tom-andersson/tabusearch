9e3e0f6728ffffde6950294ff34157d12b137287
Tom Andersson <you@example.com>
2019-01-10 20:27:49 +0000

    Made Tabu class instantiatable for guaranteed independence between runs. Made experiments in main method more clear.

--- Java project/src/tabusearch/TSMain.java
+++ Java project/src/tabusearch/TSMain.java
@@ -14,72 +14,121 @@ import org.json.simple.JSONObject;
 public class TSMain {
 
 	// Main method
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws CloneNotSupportedException, IOException {
 
-		// Using polymorphism to allow for the objective functions to be easily changed here
-		Function myFunc = new Schwef();
+		// Setting up the objective function to minimise
+		Function myFunc = new Schwef(); // Using polymorphism to allow for the objective function to be easily changed here
 		final int dim = 5; // Input dimension
 		myFunc.setDim(dim); // Set static dimension variable of the the SchwefelFunction class
 		
 		// Setting up the classes
 		Tabu.myFunc = myFunc;
 		Tabu.dim = dim;
-		// Algorithm parameters to be defined
+		// Default algorithm parameters to be defined (may be varied later in experiments)
 		Tabu.verbose = false; // True: print progress events of the search.
-		Tabu.seed = 40; // Set the rng seed
+		Tabu.seed = 50; // Set the rng seed
 		Tabu.intensifyThresh = 10; // Counter limit to intensify search using MTM
 		Tabu.diversifyThresh = 15; // Counter limit to diversify search using long-term memory (LTM)
 		Tabu.ssrThresh = 25; // Counter limit to perform step-size reduction
-		Tabu.stepSize = 25; // Starting step size for the Tabu local search
+		Tabu.startingStepSize = 175; // Starting step size for the Tabu local search
 		Tabu.stepLimit = 1E-13; // Lower limit on the step size (>1E-13 to prevent double precision issues)
-		Tabu.stepReduceFactor = 0.75; // Constant factor to reduce stepSize by after step-size reduction
-		Tabu.eval_limit = 10000; // Convergence criterion - stop when num_evals reaches eval_limit
-		LocalSearch.STM.stmSize = 8; // Short-term memory (STM) size
+		Tabu.stepReduceFactor = 0.985; // Constant factor to reduce stepSize by after step-size reduction
+		Tabu.evalLimit = 10000; // Convergence criterion - stop when numEvals reaches evalLimit
+		Tabu.stmSize = 8; // Short-term memory (STM) size
 		MTM.mtmSize = 4; // Medium-term memory (MTM) size
 		Tabu.constraint = 500.0; // Upper limit on variable magnitude
 		LTM.setSegSize(100.0); // Long-term memory (LTM) segment size for grid
+	
 		
+		// EXPERIMENTS FOR PYTHON USING JSON FILES
 		
-		// TEMP EXPERIMENTS FOR PYTHON USING JSON FILES
+		// Choose whether to save the data
+		boolean saveData = true;
+		// Choose the filename of saveData = true
+		String jsonFilename = "counterthresholds.json"; 
 		
-		// Perform a loop of Tabu searches
-		int nTabuRuns = 1; // Number of iterations to average over with different random seeds
-		// Nested array of zero-held function evolutions
-		ArrayList<LinkedList<Double>> allZeroHeldHistories = new ArrayList<LinkedList<Double>>(nTabuRuns);
-		ArrayList<LinkedList<Double>> allZeroHeldMinVals = new ArrayList<LinkedList<Double>>(nTabuRuns);
-		Tabu.seed = 1; // Initial seed
-		System.out.println("Starting a loop of Tabu runs");
-		for (int k = 0; k < nTabuRuns; k++) {
-			Tabu.doTabuSearch(); 
-			allZeroHeldHistories.add(Tabu.globfEvolZeroHold);
-			allZeroHeldMinVals.add(Tabu.globMinValZeroHold);
-			Tabu.seed++;
-		}
-
-		// Save the search data to a .json file for analysis with Python
-		System.out.println("\nSearch completed. Saving the data to a .json file.");
-		
-		String jsonFilename = "mydata.json";
+		// Choose which experiment to do
+		boolean doExperiment1 = false;
+		boolean doExperiment2 = false;
+		boolean doExperimetn3 = true;
 		
 		JSONObject jsonObj = new JSONObject();
-		List<String> tabuPath = Tabu.globSearchHist.stream().map(Point::getStringx).collect(Collectors.toList());
-		List<String> fEvolution = Tabu.globSearchHist.stream().map(Point::getStringFval).collect(Collectors.toList());
-		//jsonObj.put("tabu_path", tabuPath);
-		//jsonObj.put("f_evolution", fEvolution);
-		//jsonObj.put("num_eval_evolution", Tabu.numEvalEvolution);
-		jsonObj.put("all_zero_held_evols", allZeroHeldHistories); 
-		jsonObj.put("all_zero_held_minvals", allZeroHeldMinVals); 
-		//String jsonFilename = "Seed" + String.valueOf(Tabu.seed) + "Data.json";
+		System.out.println("Starting experiment.");
 		
-		String workingdir = System.getProperty("user.dir");
-		String parentdir = workingdir.substring(0,workingdir.lastIndexOf('\\'));
-		String jsondir = parentdir + "\\json\\";
-		
-		try (FileWriter file = new FileWriter(jsondir + jsonFilename)) {
-			file.write(jsonObj.toJSONString());
+		// Store the search path followed and the evolution of the function value for a single run
+		if (doExperiment1 == true) {
+			Tabu tabuObj = new Tabu();
+			tabuObj.doTabuSearch();
+			List<String> tabuPath = tabuObj.globSearchHist.stream().map(Point::getStringx).collect(Collectors.toList());
+			List<String> fEvolution = tabuObj.globSearchHist.stream().map(Point::getStringFval).collect(Collectors.toList());
+			jsonObj.put("tabu_path", tabuPath);
+			jsonObj.put("f_evolution", fEvolution);
+			jsonObj.put("num_eval_evolution", tabuObj.numEvalEvolution);
 		}
+		
+		// Store the (zero held) evolution of the function value and the minimum value found versus number of function evaluations
+		// for a number of different seeds (to be averaged in Python)
+		if (doExperiment2 == true) {
+			int nTabuRuns = 20; // Number of iterations to average over with different random seeds
+			// Nested array of zero-held function evolutions
+			ArrayList<LinkedList<Double>> allZeroHeldHistories = new ArrayList<LinkedList<Double>>(nTabuRuns);
+			ArrayList<LinkedList<Double>> allZeroHeldMinVals = new ArrayList<LinkedList<Double>>(nTabuRuns);
+			Tabu.seed = 1; // Initial seed
+			for (int k = 0; k < nTabuRuns; k++) {
+				Tabu tabuObj = new Tabu();
+				tabuObj.doTabuSearch(); 
+				allZeroHeldHistories.add(tabuObj.globfEvolZeroHold);
+				allZeroHeldMinVals.add(tabuObj.globMinValZeroHold);
+				Tabu.seed+=1;
+			}
+			jsonObj.put("all_zero_held_evols", allZeroHeldHistories); 
+			jsonObj.put("all_zero_held_minvals", allZeroHeldMinVals); 
+		}
+		
+		// Vary a Tabu search parameter and store linked lists of the best objectives found over many random seeds
+		// (to be averaged in Python)
+		if (doExperimetn3 == true) {
+			int nTabuRuns = 50; // Number of iterations to average over with different random seeds
+			ArrayList<ArrayList<Double>> bestSolutions = new ArrayList<ArrayList<Double>>(nTabuRuns);
+			ArrayList<Double> parameterValues = new ArrayList<Double>(nTabuRuns);
+			ArrayList<Double> currentBestMinVals; // Min vals for the current parameter value
+			for (int l = 1; l <50; l++) { // Range of parameter values
+				currentBestMinVals = new ArrayList<Double>(nTabuRuns);
+				Tabu.seed = 1; // Initial seed
+				Tabu.intensifyThresh = l;
+				Tabu.diversifyThresh = Tabu.intensifyThresh + 5;
+				Tabu.ssrThresh = Tabu.intensifyThresh + 15;
 
-		System.out.println("Json file saved.");
+				for (int k = 0; k < nTabuRuns; k++) {
+					Tabu tabuObj = new Tabu();
+					tabuObj.doTabuSearch(); 
+					System.out.println(tabuObj.bestSolution.fval);
+					currentBestMinVals.add(tabuObj.bestSolution.fval);
+					Tabu.seed+=1;
+				}
+				parameterValues.add((double) Tabu.intensifyThresh);
+				bestSolutions.add(currentBestMinVals);
+			}
+			jsonObj.put("parameter", parameterValues);
+			jsonObj.put("best_solus", bestSolutions);
+		}
+		
+		System.out.println("\nExperiment completed.");
+		
+		if (saveData == true) {
+			// Save the search data to a .json file for analysis with Python
+			System.out.println("Saving the data to a .json file.");
+			
+			String workingdir = System.getProperty("user.dir");
+			String parentdir = workingdir.substring(0,workingdir.lastIndexOf('\\'));
+			String jsondir = parentdir + "\\json\\";
+			
+			try (FileWriter file = new FileWriter(jsondir + jsonFilename)) {
+				file.write(jsonObj.toJSONString());
+			}
+
+			System.out.println("Json file saved.");
+		}
 	}
-
 }
--- Java project/src/tabusearch/Tabu.java
+++ Java project/src/tabusearch/Tabu.java
@@ -8,43 +8,290 @@ import java.util.Random;
 // and static methods for performing typical processes of Tabu search.
 public class Tabu {
 	
+	
+	
+	// Inner class for performing a local search (with access to all enclosing class members)
+	class LocalSearch {
+		
+		
+		
+		// Inner class for the short-term memory (STM)
+		class STM {
+			public int stmSize = Tabu.stmSize; // STM size
+			private LinkedList<Point> stmList = new LinkedList<Point>(); // LinkedList variable
+			
+			public void tryAddToSTM(Point currentPoint) throws CloneNotSupportedException {
+				// If the STM is not yet full
+				if (stmList.size() < stmSize) { 
+					stmList.add(currentPoint.clone()); // Add the current point to the STM
+				}
+				else {
+					stmList.offerFirst(currentPoint.clone());
+					stmList.removeLast();
+				} // Replace with the current point in the STM on a first in, first out basis
+			}
+		}
+		
+		
+		
+		// LocalSearch members ----------------------------------------------------------------------------------------
+		
+		public LinkedList<Point> localSearchHist = new LinkedList<Point>(); // List to store history of points in the local search
+		// Local history of objective function value zero-held with # of function evaluations
+		public LinkedList<Double> localfEvolZeroHold = new LinkedList<Double>(); 
+		STM stmObj = new STM(); 
+		private double stepSize; // Step size 
+		private Point currentPoint; // Point object corresponding to the current position of the local search
+		private int nEvalsLastIter; // Number of function evaluations during the previous iteration
+		private int num_its = 0; // Total number of local search iterations
+
+		// Constructor for creating a LocalSearch object
+		public LocalSearch(double stepSize) {
+			this.stepSize = stepSize;
+		}
+
+		// Generate a linked list of points around 'currentPoint' to test
+		// This is done by incrementing and decrementing each variable of the current point
+		// The testList object is reused by reference to avoid frequent expensive object creation 
+		private void updateTestList(LinkedList<double[]> testList, Point currentPoint, double stepSize) {
+			testList.clear(); // Remove all elements from the list
+			
+			// Populate the list
+			for (int i = 0; i < currentPoint.x.length; i++) {
+				double[] xTemp1 = currentPoint.x.clone();
+				xTemp1[i] += stepSize;
+				testList.add(xTemp1);
+
+				double[] xTemp2 = currentPoint.x.clone();
+				xTemp2[i] -= stepSize;
+				testList.add(xTemp2);
+			}
+		}
+		
+		// Generate list to store the permitted non-tabu moves that are within the feasible region
+		// The validList object is reused by reference to avoid frequent expensive object creation 
+		private void updateValidList(LinkedList<double[]> testList,LinkedList<Point> validList, STM STMObj) {
+			validList.clear(); // Remove all elements from the list
+			
+			for (double[] testEl : testList) {			
+				boolean validCheck1 = true; // Feasible region check
+				for (int i = 0; i < testEl.length; i++) {
+					if (Math.abs(testEl[i]) > constraint) {
+						validCheck1 = false;
+					} // "If any of the variables violate the constraints"
+				}
+				
+				boolean validCheck2 = true; // Non-tabu check
+				if (validCheck1 == true) {
+					for (Point stmEl : STMObj.stmList) {
+						if (Arrays.equals(testEl,stmEl.x)) {
+							validCheck2 = false;
+						} // "If the test point is in the STM"
+					}
+				}
+				
+				if (validCheck1==true && validCheck2==true) {
+					validList.add(new Point(testEl,myFunc));
+				}
+			}
+		}
+		
+		// Make the best allowed move of the current and return true of the objective function was reduced
+		private boolean makeBestAllowedMove(LinkedList<Point> validList) throws CloneNotSupportedException {
+			boolean functionReduced = false;
+			double prevFval = currentPoint.fval;
+			
+			if (validList.peekFirst() == null) {
+				// This is very unlikely to occur unless the input space is 1D 
+				// and the search has reached the feasible region boundary
+				// or if the stepSize is below double precision (~1E-16)
+				System.out.println("Error: There are no valid, non-tabu moves in the current local search.");
+				System.exit(0); 
+			}
+			else {
+				// Find the element with minimum function value
+				double min = validList.peekFirst().fval;
+				int minloc = 0;
+				for (int i = 0; i < validList.size(); i++) {
+					Point p = validList.get(i);
+					if (p.fval < min) {
+						min = p.fval;
+						minloc = i; 
+					}
+				}
+				currentPoint = validList.get(minloc).clone(); // Move the current point to the best valid position
+				
+				// If the function is reduced by the step, see if it is the new local minimum value
+				if (currentPoint.fval < prevFval) {
+					functionReduced = true; 
+				}
+			}
+			
+			return functionReduced;
+		}
+		
+
+		// Attempt a pattern move and execute it if it improves upon the minimum of the local search
+		private void attemptPatternMove(double[] xBase, Point currentPoint, STM stmObj) throws CloneNotSupportedException {
+			double[] xCurrent = currentPoint.x;
+			double[] xPattern = new double[xCurrent.length];
+			
+			// Perform a pattern move by repeating the vector from the last base point
+			for (int i = 0; i < xPattern.length; i++) {
+				xPattern[i] = xCurrent[i] + (xCurrent[i] - xBase[i]);
+			}
+			
+			// Check whether the pattern move is still in the feasible region
+			boolean validCheck = true; // Feasible region check
+			for (int i = 0; i < xPattern.length; i++) {
+				if (Math.abs(xPattern[i]) > constraint) {
+					validCheck = false;
+				}
+			}
+			
+			if (validCheck == true) {
+				// If the objective function is reduced, store the intermediate Tabu move and retain the pattern move
+				double fPattern = myFunc.f(xPattern);
+				if (fPattern < currentPoint.fval) {
+					storePoint(currentPoint);
+					currentPoint = new Point();
+					currentPoint.x = xPattern.clone();
+					currentPoint.fval = fPattern;
+				}
+			}
+		}
+		
+		// Attempt to store a clone of the current Point object in the various memory objects
+		private void storePoint(Point currentPoint) throws CloneNotSupportedException {
+			// Update best solution if the best objective has been reduced
+			checkBestSolution(currentPoint);
+
+			// Zero hold the function evolution and best solution values by 
+			// the number of evaluations since the last point was stored
+			nEvalsLastIter = numEvals - numEvalsLagged; 
+			for (int i = 0; i < nEvalsLastIter; i++) {
+				localfEvolZeroHold.add(currentPoint.fval); // Add the current point to the local search history
+				globMinValZeroHold.add(bestSolution.fval); // Add the current optimal solution
+			}
+			
+			localSearchHist.add(currentPoint.clone()); // Add the current point to the local search history
+			// See if the point should be stored in the STM, the MTM or the global minimum
+			stmObj.tryAddToSTM(currentPoint);
+			mtmObj.tryAddToMTM(currentPoint);
+			// Attempt to store current point in LTM after an integer number of iterations,
+			// where the integer is the number of Tabu steps that fit within each grid segment.
+			if (num_its % ltmUpdateRate == 0) {
+				ltmObj.storeInLTM(currentPoint.x);
+			}
+			numEvalEvolution.add(numEvals); // Record number of objective function evaluations
+			
+			numEvalsLagged = numEvals; // Number of evaluations since previous stored point
+		}
+		
+		// Perform a local search starting at startingPoint and ending when the counter threshold
+		// has been reached
+		public void doLocalSearch(Point startingPoint) throws CloneNotSupportedException {
+					
+			// Initialisation
+			currentPoint = startingPoint.clone(); // Generate the initial point
+			storePoint(currentPoint);
+			
+			LinkedList<double[]> testList = new LinkedList<double[]>(); // List of coordinates to check for validity
+			LinkedList<Point> validList = new LinkedList<Point>(); // List of points corresponding to valid Tabu moves 
+			int counterThresh; // Counter limit for this local search
+			
+			if (searchType.matches("initialise|ssr")) {
+				counterThresh = intensifyThresh;
+			}
+			else if (searchType.equals("intensify")) {
+				counterThresh = diversifyThresh;
+			}
+			else {
+				counterThresh = ssrThresh;
+			}
+			
+			// Begin local search
+			while (counter < counterThresh) {
+				if (verbose == true) {
+					System.out.print(counter + " ");
+				}
+				
+				double[] xBase = currentPoint.x.clone(); // Update the base point
+				updateTestList(testList,currentPoint,stepSize);	
+				updateValidList(testList,validList,stmObj);	
+				boolean functionReduced = makeBestAllowedMove(validList); // Move currentPoint by best allowed move
+
+				// Reset the counter if a new global minimum value has been found
+				if (currentPoint.fval < bestSolution.fval) {
+					counter = 0; 
+				}
+				else {
+					counter += 1; // Increment the counter
+				}
+				
+				storePoint(currentPoint);
+				
+				// If the objective function was reduced, attempt a pattern move
+				if (functionReduced == true) {
+					attemptPatternMove(xBase, currentPoint, stmObj);
+				}
+				
+				num_its++;
+				if (num_its > 1000000) {
+					System.out.println("Error: Excessive number of local search iterations reached without triggering counter threshold.");
+					System.exit(0);
+				}
+				
+			}
+			
+			if (searchType.matches("initialise|ssr")) {
+				searchType = "intensify";
+			}
+			else if (searchType.equals("intensify")) {
+				searchType = "diversify";
+			}
+			else {
+				searchType = "ssr";
+			}
+		}
+	}
+	
+	
+	
+	//// Tabu members ----------------------------------------------------------------------------------------
+	
+	// Shared static variables
 	public static boolean verbose; // True: print progress events of the search.
-	public static int numEvals = 0; // Counter for number of objective function evaluations
-	public static int numEvalsLagged = 0; // Number of function evaluations up to the previous Tabu iteration
-	public static LinkedList<Integer> numEvalEvolution; // Evolution of the number of function evaluations
-	public static int eval_limit; // Convergence criterion on the number of objective function evaluations permitted
-	public static LinkedList<Point> globSearchHist; // Object to store the entire Tabu search history as a linked list
-	public static LinkedList<Double> globfEvolZeroHold; // History zero held with # of function evaluations
-	public static LinkedList<Double> globMinValZeroHold; // History of optimal solution found so far (zero held)
-	public static Point globalMinPoint; // Current Point with the minimum associate objective function value
-	public static Point startingPoint; // Force the local search to begin from this point
+	public static int evalLimit; // Convergence criterion on the number of objective function evaluations permitted
 	public static int dim; // Input dimension
 	public static double constraint; // Upper limit on variable magnitude
 	public static Function myFunc; // Function to minimise
-	public static double stepSize; // Starting step size for the Tabu local search
+	public static double startingStepSize; // Starting step size for the Tabu local search
 	public static double stepLimit; // Lower limit on the step size
 	public static double stepReduceFactor; // Constant factor to reduce the step size by after step-size reduction 
-	public static Random generator; // Random generator (shared across the package)
 	public static long seed; // Rng seed
-	public static MTM mtmObj;
-	public static LTM ltmObj;
-	public static int ltmUpdateRate; // Local search iterations per update to the LTM
-	public static String searchType; // Type of local search to conduct: "initial", "intensify", "diversify" or "ssr"
-	public static int counter = 0; // Counter for number of iterations without improvement to minimum value found
+	public static int stmSize; // Short-term memory (STM) size
 	public static int intensifyThresh; // Counter threshold to intensify search
 	public static int diversifyThresh; // Counter threshold to diversify search
 	public static int ssrThresh; // Counter threshold to perform step-size reduction
-	
-	// Check whether this point corresponds the best solution found so far and store it if so 
-	public static void checkGlobalMin(Point currentPoint) throws CloneNotSupportedException {
-		if (globalMinPoint == null) {
-			globalMinPoint = currentPoint.clone();
-		}
-		else if (currentPoint.fval < globalMinPoint.fval) {
-			globalMinPoint = currentPoint.clone();
-		} 
-	}
-	
+	public static Random generator; // Random generator (shared across the package)
+	public static int numEvals; // Counter for number of objective function evaluations
+
+	// Instance variables
+	private int ltmUpdateRate; // Local search iterations per update to the LTM
+	public Point bestSolution; // Current Point with the minimum associate objective function value
+	private Point startingPoint = new Point(); // Force the local search to begin from this point
+	public double stepSize; // Variable step size
+	private MTM mtmObj = new MTM();
+	private LTM ltmObj = new LTM();
+	private int numEvalsLagged = 0; // Number of function evaluations up to the previous Tabu iteration
+	private String searchType; // Type of local search to conduct: "initial", "intensify", "diversify" or "ssr"
+	public LinkedList<Point> globSearchHist = new LinkedList<Point>(); // Object to store the entire Tabu search history as a linked list
+	public LinkedList<Double> globfEvolZeroHold= new LinkedList<Double>(); // History zero held with # of function evaluations
+	public LinkedList<Integer> numEvalEvolution = new LinkedList<Integer>(); // Evolution of the number of function evaluations
+	public LinkedList<Double> globMinValZeroHold = new LinkedList<Double>(); // History of optimal solution found so far (zero held)
+	private int counter; // Counter for number of iterations without improvement to minimum value found
+
 	// Generate a random input point
 	public static Point genRandomPoint() {
 		double[] x = new double[dim];
@@ -55,35 +302,40 @@ public class Tabu {
 		return new Point(x, myFunc);
 	}
 	
+	// Check whether this point corresponds the best solution found so far and store it if so 
+	private void checkBestSolution(Point currentPoint) throws CloneNotSupportedException {
+		if (bestSolution == null) {
+			bestSolution = currentPoint.clone();
+		}
+		else if (currentPoint.fval < bestSolution.fval) {
+			bestSolution = currentPoint.clone();
+		} 
+	}
+	
 	// Perform a complete Tabu search
-	public static void doTabuSearch() throws CloneNotSupportedException {
+	public void doTabuSearch() throws CloneNotSupportedException {
 		// Initialisation
-		globSearchHist = new LinkedList<Point>(); 
-		globfEvolZeroHold = new LinkedList<Double>();
-		globMinValZeroHold = new LinkedList<Double>();
-		mtmObj = new MTM();
-		ltmObj = new LTM();
-		numEvalEvolution = new LinkedList<Integer>();
 		generator = new Random(seed);
 		numEvals = 0;
 		counter = 0; 
+		stepSize = startingStepSize;
 		ltmUpdateRate = (int) Math.ceil(LTM.getSegSize()/(2*stepSize));
 		searchType = "initialise";
 		if (verbose == true) {
-			System.out.println("Starting a Tabu search. Printing counter evolution and other search events.");
+			System.out.println("\n\nStarting a Tabu search. Printing counter evolution and other search events.");
 		}
 		
 		startingPoint = genRandomPoint(); 
 		
-		while (numEvals < eval_limit) {
+		while (numEvals < evalLimit) {
 			if (searchType.equals("initialise") && verbose == true) {
-				System.out.print("\nInitial search: ");
+				System.out.print("Initial search: ");
 			}
 			
 			// Intensify search
 			if (searchType.equals("intensify")) {
 				startingPoint = MTM.findMTMAvg(mtmObj);
-				if (Tabu.verbose == true) {
+				if (verbose == true) {
 					System.out.print("\nIntensiying search: ");
 				}
 			} // Intensify: set starting point to the meant point of the MTM
@@ -95,14 +347,14 @@ public class Tabu {
 			
 			// Reduce step size
 			else if (searchType.equals("ssr")) {
-				startingPoint = globalMinPoint; // Restart from the minimum point found so far
+				startingPoint = bestSolution; // Restart from the minimum point found so far
 				// If the step size limit will not be violated, reduce the step size
 				if (stepSize*stepReduceFactor > stepLimit) {
 					stepSize = stepSize*stepReduceFactor; // Reduce the increment size by a constant factor
 				}
 				counter = 0; // Reset counter
 				ltmUpdateRate = (int) Math.ceil(LTM.getSegSize()/(2*stepSize));
-				if (Tabu.verbose == true) {
+				if (verbose == true) {
 					System.out.print("\nReducing step size: ");
 				}
 			}
@@ -110,9 +362,8 @@ public class Tabu {
 			// Perform a local search from startingPoint
 			LocalSearch LSObj = new LocalSearch(stepSize); 
 			LSObj.doLocalSearch(startingPoint); 
-			globSearchHist.addAll(LocalSearch.localSearchHist); // Append the local search history of points to the global search history
-			globfEvolZeroHold.addAll(LocalSearch.localfEvolZeroHold); // Append zero held history
+			globSearchHist.addAll(LSObj.localSearchHist); // Append the local search history of points to the global search history
+			globfEvolZeroHold.addAll(LSObj.localfEvolZeroHold); // Append zero-held objective history
 		}
 	}
-
 }
